da47f319ffab2c02bb979f0c645c406debe47ed9
Athir Nuaimi <athir@nuaimi.com>
2014-03-16 21:42:44 -0400

    update jenkins install script.  still not complete

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ this will be a project to create all the core services needed to properly and pr
 
 assumes all servers based on Ubuntu 12.04 LTS
 
+need a repo for each tool
+ - repo for jenkins jobs
+
+
 licensed under Apache 2
--- deployment/jenkins/install.sh
+++ deployment/jenkins/install.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# based on:
+# http://gistflow.com/posts/492-jenkins-ci-setup-for-rails-application-from-scratch
+
+sudo apt-get update
+
+# install git 
+sudo apt-get install -y git-core 
+
+# install rvm
+sudo apt-get install -y curl
+curl -L https://get.rvm.io | bash -s stable --ruby
+rvm install ruby-1.9.3
+
+# install ruby 1.9.3
+# sudo apt-get install -y ruby1.9.3 rubygems1.9.1
+# sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+#          --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz \
+#                         /usr/share/man/man1/ruby1.9.1.1.gz \
+#         --slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
+#         --slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
+#         --slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
+# sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.9.1 400 
+
+
+# install capistrano 3.x
+sudo gem install bundler --no-rdoc --no-ri
+cat <<EOF > Gemfile
+source 'https://rubygems.org'
+gem 'capistrano', '~> 3.1.0' 
+EOF
+bundle install
+
 # add jenkins repo to list 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -8,7 +40,38 @@ sudo apt-get update
 # install jenkins
 # will automatically start jenkins on port 8080
 sudo apt-get install -y jenkins
+sudo service jenkins stop
+
+sudo apt-get install -y nginx
+
+
+# set admin password
+# JENKINS_ADMIN_USER=root
+# JENKINS_ADMIN_PWD=password
+# --argumentsRealm.passwd.${JENKINS_ADMIN_USER}=${JENKINS_ADMIN_PWD}
+# --argumentsRealm.roles.${JENKINS_ADMIN_USER}=admin
+
+# get plugins
+cd $JENKINS_HOME/plugins
+sudo curl -O http://updates.jenkins-ci.org/latest/git.hpi
+sudo chown jenkins:nogroup git.hpi
+curl --data "" http://localhost:8080/reload
+
+# restart jenkins
+sudo service jenkins restart
+
+sudo su jenkins
+ssh-keygen -t rsa
 
 # do we want any slaves?
 
 # get jenkins config files from git
+wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+java -jar jenkins-cli.jar -s http://localhost:8080/ help
+
+
+java -jar jenkins-cli.jar -s http://localhost:8080/ reload-configuration
+
+# API calls
+# -add_user( username, password, role)
+# -remove_user( username)
